297ef7e76efb72a08d1da736767611bea17030c7
Echalaye <etienne.chalaye@gmail.com>
2024-04-30 09:29:57 +0200

    prend

--- oiram/Physics/Rigidbody/Rigidbody.cpp
+++ oiram/Physics/Rigidbody/Rigidbody.cpp
@@ -1,6 +1,5 @@
 #include "Rigidbody.h"
 
-Engine engine;
 
 void Rigidbody::Gravity(float multiplier)
 {
--- oiram/oiram.cpp
+++ oiram/oiram.cpp
@@ -14,58 +14,30 @@ Vector2<float> C = Vector2(0.f);
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-    Engine engine;
-    Rigidbody rb;
-    
-        while (window.isOpen())
-        {
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
-            //std::cout << engine.clock.getElapsedTime().asSeconds() << "\n";        
-            
-            //rb.AddForce(Vector2<float>(.2f, .2f), Impulse);
-            
-            rb.useGravity = true;
-            rb.Gravity(.1f);
-            
-            std::cout << rb.velocity.x << " | " << rb.velocity.y << "\n";
-            
-            
-            window.clear(sf::Color::Black);
-            
-        
-            window.display();
-            engine.clock.restart();
+        WindowManager window_manager;
+        window_manager.WindowDraw();
 
-            WindowManager window_manager;
-            window_manager.WindowDraw();
+        // sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+        //
+        // while (window.isOpen())
+        // {
+        //     sf::Event event;
+        //     while (window.pollEvent(event))
+        //     {
+        //         if (event.type == sf::Event::Closed)
+        //             window.close();
+        //     }
+        //     clock.restart();
+        //
+        //     float deltaTime = clock.restart().asSeconds();
+        //     
+        //     window.clear(sf::Color::Black);
+        //     
+        //
+        //     window.display();
+        // }
     
-            // sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-            //
-            // while (window.isOpen())
-            // {
-            //     sf::Event event;
-            //     while (window.pollEvent(event))
-            //     {
-            //         if (event.type == sf::Event::Closed)
-            //             window.close();
-            //     }
-            //     clock.restart();
-            //
-            //     float deltaTime = clock.restart().asSeconds();
-            //     
-            //     window.clear(sf::Color::Black);
-            //     
-            //
-            //     window.display();
-            // }
-        }
 
     return 0;
 }
