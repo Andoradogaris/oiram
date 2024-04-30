ff3bf74c59405352f2910f2dce5fd5da458c46ad
Echalaye <etienne.chalaye@gmail.com>
2024-04-30 09:39:21 +0200

    prend

--- oiram/oiram.cpp
+++ oiram/oiram.cpp
@@ -15,28 +15,28 @@ Vector2<float> C = Vector2(0.f);
 int main()
 {
 
-        WindowManager window_manager;
-        window_manager.WindowDraw();
+    WindowManager* window_manager = ObjectManager::Get()->CastCreateObject<WindowManager>(WindowManager::ClassName());
+    window_manager->WindowDraw();
 
-        // sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-        //
-        // while (window.isOpen())
-        // {
-        //     sf::Event event;
-        //     while (window.pollEvent(event))
-        //     {
-        //         if (event.type == sf::Event::Closed)
-        //             window.close();
-        //     }
-        //     clock.restart();
-        //
-        //     float deltaTime = clock.restart().asSeconds();
-        //     
-        //     window.clear(sf::Color::Black);
-        //     
-        //
-        //     window.display();
-        // }
+    // sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    //
+    // while (window.isOpen())
+    // {
+    //     sf::Event event;
+    //     while (window.pollEvent(event))
+    //     {
+    //         if (event.type == sf::Event::Closed)
+    //             window.close();
+    //     }
+    //     clock.restart();
+    //
+    //     float deltaTime = clock.restart().asSeconds();
+    //     
+    //     window.clear(sf::Color::Black);
+    //     
+    //
+    //     window.display();
+    // }
     
 
     return 0;
